643644fc409baa3d3951dac5110222eadb2cf69a
NRCTL <bongnerm@gmail.com>
2026-07-16 21:12:18 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const mainNav = document.querySelector(".main-nav");
-
-navToggle.addEventListener("click", () => {
-  mainNav.classList.toggle("active");
-});
